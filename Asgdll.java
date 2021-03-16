08f9b5d66193410450600183dadb76ebfc3115c5
Dimesss <61872020+Dimesss@users.noreply.github.com>
2021-03-16 23:27:35 +0800

    Add files via upload

---
+++ Asgdll.java
@@ -0,0 +1,9 @@
+public class Asgdll {
+	public static void main(String [] args) {
+		float f= 20f;
+		double fll;		
+		fll=10f;
+		System.out.println ("f  : "+f  + "\nfll: "+fll);
+	}
+
+}
---
+++ Ekspresi.java
@@ -0,0 +1,18 @@
+/* pemakaian operator kondisional */
+	public class Ekspresi {
+/**
+* @param args
+*/
+		public static void main(String[] args) {
+
+// TODO Auto-generated method stub
+/* KAMUS */
+			int x = 3;
+			int y = 1;
+/* ALGORITMA */
+		System.out.print("x = "+ x + "\n");
+		System.out.print("y = "+ y + "\n");
+		System.out.print("hasil ekspresi = (x<y)?x:y = "+ ((x < y) ? x : y)); 
+	/*Gunakan dalam kurung "(statemen dan kondisi)" untuk menyatakan satu kesatuan pernyataan*/
+	}
+}
